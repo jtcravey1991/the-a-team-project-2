f2d26792f2ec215471d0dab178dc308099ea1ac1
Linnea <linneagear@gmail.com>
2020-07-13 21:53:47 -0700

    Fixed handlebars file structure

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,40 +5,7 @@ const path = require("path");
 //const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // app.get("/", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.render("signup", {});
-  // });
-
-  // app.get("/login", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.render("login", {});
-  // });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // app.get("/members", isAuthenticated, (req, res) => {
-  //   res.render("members", {});
-  // });
-
-  // blank route loads index.html
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
-  //route to log.html
-  app.get("/log", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/log.html"));
-  });
-
-  // goals route loads goals.html
-  app.get("/goals", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/goals.html"));
+    res.render("signup", {});
   });
 };
