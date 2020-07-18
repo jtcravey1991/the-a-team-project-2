89c0ecb673073d9c6f674e656b8612366b81d1ca
Jake <j.t.cravey1991@gmail.com>
2020-07-18 14:21:57 -0700

    another attempted fix

--- public/js/goalSetter.js
+++ public/js/goalSetter.js
@@ -1,7 +1,7 @@
-$(".plus-minus").on("change", () => {
-  if (this.is(":checked")) {
+$(".plus-minus").on("change", (event) => {
+  if (this.prop("checked")) {
     this.parent().parent().addClass("goal__checked");
-  } else if (!this.is(":checked")) {
+  } else if (!this.prop("checked")) {
     this.parent().parent().removeClass("goal__checked");
   }
 });
